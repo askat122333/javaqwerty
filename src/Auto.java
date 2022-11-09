14a5801742d3538389e8f69c94d9719f2ac52a3c
askat122333 <askatalymbaev33@gmail.com>
2022-11-09 17:59:00 +0600

    first commit

---
+++ src/Auto.java
@@ -0,0 +1,15 @@
+public class Auto {
+    String name;
+
+    public Auto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
